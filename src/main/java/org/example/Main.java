ebd9188fe74b64ae95e1ff8de113f37b739a0bd1
Ceyhan Fazlıoğlu <ceyhanfazlioglu@gmail.com>
2026-06-02 22:54:15 +0300

    v

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,23 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("--- Point Tests ---");
+        Point first = new Point(6, 5);
+        Point second = new Point(3, 1);
+        System.out.println("distance(0,0)= " + first.distance());
+        System.out.println("distance(second)= " + first.distance(second));
+        System.out.println("distance(2,2)= " + first.distance(2, 2));
+
+        System.out.println("\n--- Player Tests ---");
+        Player player = new Player("Arthur", 90, Weapon.SWORD);
+        System.out.println("Initial Health: " + player.healthRemaining());
+
+        player.loseHealth(40);
+        System.out.println("After Damage: " + player.healthRemaining());
+
+        player.restoreHealth(20);
+        System.out.println("After Potion: " + player.healthRemaining());
+
+        player.loseHealth(80);
     }
 }
